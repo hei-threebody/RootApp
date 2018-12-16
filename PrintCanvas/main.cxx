bf67e3ebfda4fa7a38c30dcadcc4434a06dc1ae8
Qiutum <zh4710jj@mail.ustc.edu.cn>
2018-12-16 18:33:45 +0800

    e

--- PrintCanvas/main.cxx
+++ PrintCanvas/main.cxx
@@ -26,42 +26,42 @@ void PrintCanvas(char *s) {
 	//THStack hs("hs", "test stacked histograms");
 	TCanvas *c1 = new TCanvas("c1", "c1", 2000, 2000);
 	c1->Divide(2, 2);
-	TTree *kmfit = (TTree *) f->Get("kmfit");
+	TTree *PrSth = (TTree *) f->Get("PrSth");
 
-	Double_t lamd, lamdbar, sigma0, sigma1;
-	kmfit->SetBranchAddress("lamd", &lamd);
-	kmfit->SetBranchAddress("lamdbar", &lamdbar);
-	kmfit->SetBranchAddress("sigma0", &sigma0);
-	kmfit->SetBranchAddress("sigma1", &sigma1);
-	TH1F *lamdHF    = new TH1F("lamd", "#lambda invariant mass", 100, 1.08, 1.18);
-	TH1F *lamdbarHF = new TH1F("lamdbar", "#bar{#lambda} invariant mass", 100, 1.08, 1.18);
-	TH1F *sigma0HF  = new TH1F("sigma0", "#Sigma^{0}(#lambda + #gamma) invariant mass", 100, 1.1, 1.7);
-	TH1F *sigma1HF  = new TH1F("sigma1", "#Sigma^{0}(#bar{#lambda} + #gamma) invariant mass", 100, 1.08, 1.3);
+	Double_t lmdmas, lmdmasbar, sigmas, sigmasbar;
+	PrSth->SetBranchAddress("lmdmas", &lmdmas);
+	PrSth->SetBranchAddress("lmdmasbar", &lmdmasbar);
+	PrSth->SetBranchAddress("sigmas", &sigmas);
+	PrSth->SetBranchAddress("sigmasbar", &sigmasbar);
+	TH1F *lmdmasHF    = new TH1F("lmdmas", "#lambda invariant mass", 100, 1.08, 1.18);
+	TH1F *lmdmasbarHF = new TH1F("lmdmasbar", "#bar{#lambda} invariant mass", 100, 1.08, 1.18);
+	TH1F *sigmasHF  = new TH1F("sigmas", "#Sigma^{0}(#lambda + #gamma) invariant mass", 100, 1.1, 1.7);
+	TH1F *sigmasbarHF  = new TH1F("sigmasbar", "#Sigma^{0}(#bar{#lambda} + #gamma) invariant mass", 100, 1.08, 1.3);
 
-	for (Int_t i = 0; i < (Int_t) kmfit->GetEntries(); i++) {
-		kmfit->GetEntry(i);
-		lamdHF->Fill(lamd);
-		lamdbarHF->Fill(lamdbar);
-		sigma0HF->Fill(sigma0);
-		sigma1HF->Fill(sigma1);
+	for (Int_t i = 0; i < (Int_t) PrSth->GetEntries(); i++) {
+		PrSth->GetEntry(i);
+		lmdmasHF->Fill(lmdmas);
+		lmdmasbarHF->Fill(lmdmasbar);
+		sigmasHF->Fill(sigmas);
+		sigmasbarHF->Fill(sigmasbar);
 
 	}
 
-	//hs.Add(lamdHF);
-	//hs.Add(lamdbarHF);
-	//hs.Add(sigma1HF);
+	//hs.Add(lmdmasHF);
+	//hs.Add(lmdmasbarHF);
+	//hs.Add(sigmasbarHF);
 
 	//hs.Draw("nostack");
 	//hs.Print("hs.pdf");
 	//
 	c1->cd(1);
-	lamdHF->Draw();
+	lmdmasHF->Draw();
 	c1->cd(2);
-	lamdbarHF->Draw();
+	lmdmasbarHF->Draw();
 	c1->cd(3);
-	sigma0HF->Draw();
+	sigmasHF->Draw();
 	c1->cd(4);
-	sigma1HF->Draw();
+	sigmasbarHF->Draw();
 	c1->Print("tmp.pdf");
 	//c1->Draw();
 }
